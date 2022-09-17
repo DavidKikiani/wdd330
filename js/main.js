1a3b14fed3b44e4239afae801162c32c0b798e58
David Kikiani <davidkikiani@mail.com>
2022-09-17 13:46:29 +0400

    Week 1 assignment

---
+++ js/main.js
@@ -0,0 +1,32 @@
+// const links = [{
+//     label: "Week1 notes",
+//     url: "week1/index.html"
+// }];
+
+// function makeList(){
+
+// var listContainer = document.createElement('div');
+// var listElement = document.createElement('ol');
+// var numListItems = links.length;
+// var listItem;
+// var i;
+
+// document.getElementsByTagName('body')[0].appendChild(listContainer);
+// listContainer.appendChild(listElement);
+
+// for (i = 0; i < numListItems; i++) {
+// //create line items, populate them, and add list item to list element
+//     listItem = document.createElement('li');
+//     listItem.innerHTML = links[i];
+//     listElement.appendChild(listItem);
+    
+// }
+// }
+
+
+var li = document.createElement('li'),
+        anchor = document.createElement('a');
+        anchor.href = a.url;
+        anchor.text = a.name;
+        li.appendChild(anchor);
+        ul.appendChild(li);
